e83d98bc3b72a7a55635afeff5b756ed2b8f7112
Surya <thapasuryabahadur869@gmail.com>
2026-09-07 13:03:48 +0545

    first-commit

---
+++ Script.js
@@ -0,0 +1,24 @@
+function validateForm(){
+
+document.getElementById("emailError").innerHTML="";
+document.getElementById("passError").innerHTML="";
+
+let email=document.getElementById("email").value;
+let pass=document.getElementById("password").value;
+
+if(email.indexOf("@")==-1){
+document.getElementById("emailError").innerHTML="Email must contain @.";
+valid=false;
+}
+
+if(pass.length<8){
+document.getElementById("passError").innerHTML="Password must be at least 8 characters.";
+valid=false;
+}
+
+if(valid){
+alert("Login Successful");
+}
+
+return false;
+}
